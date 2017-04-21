12cfc9ae876f13828e0d0d15699972527ef828d8
Darcy Meyer <darcy2252755@gmail.com>
2017-04-21 17:19:32 -0400

    rename inner folder to IMEC

--- IMEC_Website/dashboard/views.py
+++ IMEC_Website/dashboard/views.py
@@ -9,7 +9,7 @@ def index(request):
 	"""
 
 	# Render the HTML template index.html with the data in the context variable
-	return render(request, 'index.html')
+	return render(request, 'dashboard/index.html')
 
 def getSearchResults(request, limit=10):
 	# try:
