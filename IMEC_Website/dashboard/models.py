064fa6a5bca0b8ca310724c20507ca77ad376e4c
Darcy Meyer <darcy2252755@gmail.com>
2017-04-22 20:02:01 -0400

    update models

--- IMEC_Website/dashboard/models.py
+++ IMEC_Website/dashboard/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.utils.timezone import localtime, now
+
+# wrapper class for User
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
 
 # Create your models here.
 class Entry(models.Model):
@@ -8,10 +15,20 @@ class Entry(models.Model):
     lotBatchNum = models.CharField(max_length=70, blank=True, null=True)
     unitsPerCase = models.CharField(max_length=70, blank=True, null=True)
     totalUnits = models.TextField(blank=True, null=True)
+    scannedBy = models.CharField(max_length=20, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
     def toDict(self):
-    	return {"catNumber": self.productID, \
-    			"siteNumber": self.description, \
-    			"locality": self.expirationDate, \
-    			"site": self.lotBatchNum, \
-    			"name": self.unitsPerCase, \
-    			"situation": self.totalUnits}
+    	return {"productID": self.productID, \
+    			"description": self.description, \
+    			"expirationDate": self.expirationDate, \
+    			"lotBatchNum": self.lotBatchNum, \
+    			"unitsPerCase": self.unitsPerCase, \
+    			"totalUnits": self.totalUnits}
+
+class VolunteerID(models.Model):
+    idString = models.CharField(max_length=20, null=True)
+    issuedBy = models.CharField(max_length=60, null=True)
+    valid = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    def isValid(self):
+        return self.valid
